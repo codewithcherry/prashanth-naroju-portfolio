493ada50e7f9ab916488ef8c1d67cc83866a3fa8
Prashanth Naroju <prashanth.naro@gmail.com>
2025-03-21 12:48:48 +0530

    fix arrow animation and position

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
     <div className="relative flex flex-col md:flex-row items-center justify-center w-4/5 mx-auto gap-8">
       <HeroIntro />
       <HeroCharacter />
-      <ArrowDown className="bg-black text-white w-10 h-10 p-1 rounded-full animate-bounce absolute bottom-40"/>
+      <ArrowDown className="bg-black text-white w-10 h-10 p-1 rounded-full animate-bounce absolute bottom-10"/>
     </div>
   );
 };
