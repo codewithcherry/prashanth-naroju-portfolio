5fecabed7ad94f19c86a2c7b4177277cd42076bc
Prashanth Naroju <prashanth.naro@gmail.com>
2025-05-03 21:14:29 +0530

    add the vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
           <Navbar />
         </div>
         {children}
+        <Analytics />
         <div className="flex justify-center">
           <Footer/>
         </div>
